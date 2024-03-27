ca99e69179e70901c37171b5864f8cc3906d6475
Kristina <kristinapejcic1994@gmail.com>
2024-03-27 16:56:19 +0100

    add main organisms components: TopNav, Hero, Footer

---
+++ frontend/src/components/organisms/TopNav.tsx
@@ -0,0 +1,46 @@
+"use client";
+import React from "react";
+
+import { AppBar, Box, Grid } from "@mui/material";
+
+import { IMAGES, PATHS, NAV_ITEMS } from "@/constants";
+import { Link } from "@/components/atoms";
+import NextLink from "next/link";
+
+export const TopNav = () => {
+  return (
+    <AppBar
+      component="nav"
+      sx={{
+        backgroundColor: "white",
+        borderColor: "lightblue",
+        borderRadius: 0,
+        boxShadow: 0,
+        justifyContent: "space-between",
+        alignItems: "center",
+        flexDirection: "row",
+        position: "relative",
+        py: { xxs: 2, sm: 3 },
+        px: { xxs: 2, sm: 5 },
+        "& img": {
+          width: "100%",
+        },
+      }}
+    >
+      <NextLink data-testid="logo-button" href={PATHS.home}>
+        <img height={35} src={IMAGES.logoSign} />
+      </NextLink>
+      <Box>
+        {NAV_ITEMS.map((navItem) => (
+          <Grid item key={navItem.label}>
+            <Link
+              data-testid={navItem.dataTestId}
+              label={navItem.label}
+              href={navItem.href}
+            />
+          </Grid>
+        ))}
+      </Box>
+    </AppBar>
+  );
+};
