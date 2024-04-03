20189783f8ec08c1ef40fa235526bc53a8affc76
Kristina <kristinapejcic1994@gmail.com>
2024-04-03 12:22:42 +0200

    feature/field-components

--- frontend/src/components/atoms/index.ts
+++ frontend/src/components/atoms/index.ts
@@ -6,5 +6,8 @@ export * from "./modal/ModalContents";
 export * from "./modal/ModalHeader";
 export * from "./modal/ModalWrapper";
 export * from "./modal/RenderModal";
+export * from "./Checkbox";
+export * from "./FormErrorMessage";
+export * from "./FormHelpfulText";
 
 export * from "./types";
