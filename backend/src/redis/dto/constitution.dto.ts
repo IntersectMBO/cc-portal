e3e9e3f0fcd6a9d58457fa2184b5179dee332e1e
Milos <161627443+BEdev24@users.noreply.github.com>
2024-04-16 14:55:05 +0200

    chore: New caching functions for constitution and diff #27

--- backend/src/redis/dto/constitution.dto.ts
+++ backend/src/redis/dto/constitution.dto.ts
@@ -1,8 +1,9 @@
 export class ConstitutionDto {
+  version: string;
   cid: string;
   content: string;
 
-  constructor(cid: string, content: string) {
+  constructor(version: string, cid: string, content: string) {
     this.cid = cid;
     this.content = content;
   }
