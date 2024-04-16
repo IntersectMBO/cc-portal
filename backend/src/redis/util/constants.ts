e0f02f48dc08f5a74a75ade23869edb999079970
bedev24 <15165767+milos1991@users.noreply.github.com>
2024-04-16 14:51:04 +0200

    feature/constitution-caching

--- backend/src/redis/util/constants.ts
+++ backend/src/redis/util/constants.ts
@@ -2,6 +2,8 @@ export class Constants {
   public static readonly PREFIX_CONSTITUTION = 'constitution';
   public static readonly SUFFIX_CURRENT_CONSTITUTION = 'current';
 
+  public static readonly PREFIX_CONSTITUTION_DIFF = 'constitution_diff';
+
   public static readonly ONE_HOUR_IN_SECONDS = 60 * 60;
   public static readonly ONE_DAY_IN_SECONDS = 60 * 60 * 24;
 }
