d2fa181a072a4952e8864221fd58b9eb80a33a85
Kristina2103 <kristinapejcic1994@gmail.com>
2024-04-25 20:09:35 +0200

    feat: Fetched and displayed constitution

--- frontend/src/lib/requests/getConstitution.ts
+++ frontend/src/lib/requests/getConstitution.ts
@@ -1,38 +1,32 @@
-import { MDXRemoteSerializeResult } from "next-mdx-remote";
 import { serialize } from "next-mdx-remote/serialize";
 import remarkToc from "remark-toc";
 import rehypeToc from "rehype-toc";
 import rehypeSlug from "rehype-slug";
+import axiosInstance from "../axiosInstance";
 
-const GITHUB_URL =
-  "https://raw.githubusercontent.com/Kristina2103/publicTestMdx/main";
+export async function getConstitution(): Promise<any> {
+  try {
+    const response: any = await axiosInstance.get("/api/constitution/current");
 
-export const CONSTITUTION_FILE = "constitution2.md";
+    if (!response || !response.contents) {
+      console.error("Invalid response format");
+      return undefined;
+    }
 
-export async function getConstitution(
-  fileName: string = CONSTITUTION_FILE
-): Promise<MDXRemoteSerializeResult | undefined> {
-  const apiUrl = `${GITHUB_URL}/${fileName}`;
+    const mdxContent = response.contents;
 
-  const res = await fetch(apiUrl);
+    const options = {
+      mdxOptions: {
+        remarkPlugins: [remarkToc],
+        rehypePlugins: [rehypeSlug, rehypeToc],
+      },
+    };
 
-  if (!res.ok) {
-    console.error("Error fetching MDX file:", res.statusText);
-    return;
+    const mdxSource = await serialize(mdxContent, options);
+
+    return mdxSource;
+  } catch (error) {
+    console.error("Error fetching and processing MDX content:", error);
+    return undefined;
   }
-
-  const rawMDX = await res.text();
-
-  if (rawMDX === "404: Not Found") return;
-
-  const options = {
-    mdxOptions: {
-      remarkPlugins: [remarkToc],
-      rehypePlugins: [rehypeSlug, rehypeToc],
-    },
-  };
-
-  const mdxSource = await serialize(rawMDX, options);
-
-  return mdxSource;
 }
