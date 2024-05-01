ad2fce0b6bd67f5643875a7ed56aec4b2b1c43dd
Milos <161627443+BEdev24@users.noreply.github.com>
2024-05-01 23:58:50 +0200

    feature/unit-tests-email-module: added unit tests for email module (#69)

--- backend/src/email/service/email.service.spec.ts
+++ backend/src/email/service/email.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EmailService } from './email.service';
 import { MailerService } from '@nestjs-modules/mailer';
+import { EmailDto } from '../dto/email.dto';
+import { InternalServerErrorException } from '@nestjs/common';
 
 describe('EmailService', () => {
   let service: EmailService;
   const mockMailerService = {
-    sendMail: jest.fn().mockResolvedValue(true), // Mock the sendMail method
+    sendMail: jest.fn(),
   };
 
   beforeEach(async () => {
@@ -28,5 +30,32 @@ describe('EmailService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('Sending email', () => {
+    it('should send an email successfully', async () => {
+      const emailDto = {
+        to: 'receiver-email',
+        subject: 'Subject title',
+      } as EmailDto;
+
+      mockMailerService.sendMail.mockResolvedValueOnce('success');
+      await service.sendEmail(emailDto);
+      expect(mockMailerService.sendMail).toHaveBeenCalledWith(emailDto);
+    });
+
+    it('should an throw error when sending an email', async () => {
+      const emailDto = {
+        to: undefined,
+        subject: undefined,
+      } as EmailDto;
+
+      mockMailerService.sendMail.mockRejectedValueOnce(
+        new InternalServerErrorException(),
+      );
+      await expect(service.sendEmail(emailDto)).rejects.toThrow(
+        InternalServerErrorException,
+      );
+    });
   });
 });
