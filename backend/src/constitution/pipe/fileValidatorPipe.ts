611c23682d553fb016ce1d6a78b9787eee18bbd6
Milos <developer@bloxico.com>
2024-05-22 15:58:52 +0200

    refactor: added exception handling when trying to upload without any file

--- backend/src/constitution/pipe/fileValidatorPipe.ts
+++ backend/src/constitution/pipe/fileValidatorPipe.ts
@@ -1,7 +1,12 @@
-import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { extname } from 'path';
 
-export function getFileExtensionValidator(): PipeTransform {
+export function getFileValidator(): PipeTransform {
   return new ParseFilePipeDocument();
 }
 
@@ -10,6 +15,9 @@ export class ParseFilePipeDocument implements PipeTransform {
   private readonly allowedExtensions = ['.md'];
 
   transform(value: Express.Multer.File): Express.Multer.File {
+    if (!value) {
+      throw new UnprocessableEntityException('File is required');
+    }
     const extension = extname(value.originalname);
     if (!this.allowedExtensions.includes(extension)) {
       throw new BadRequestException(`File type ${extension} not supported`);
