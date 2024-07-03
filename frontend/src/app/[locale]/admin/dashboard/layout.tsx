d2185ec2b6d324d421f9bd3ad0acf2b391208d42
Kristina <kristinapejcic1994@gmail.com>
2024-07-03 15:00:07 +0200

    chore: Add role and sessioin check on admin layouts

--- frontend/src/app/[locale]/admin/dashboard/layout.tsx
+++ frontend/src/app/[locale]/admin/dashboard/layout.tsx
@@ -1,7 +1,16 @@
+import { PATHS } from "@/constants";
+import { decodeUserToken } from "@/lib/api";
+import { isAnyAdminRole } from "@utils";
 import { Box } from "@mui/material";
+import { redirect } from "next/navigation";
 import React from "react";
 
-async function DashboardLayout({ children }) {
+async function DashboardLayout({ params: { locale }, children }) {
+  const user = await decodeUserToken();
+
+  if (user && !isAnyAdminRole(user?.role)) {
+    redirect(`/${locale}/${PATHS.home}`);
+  }
   return <Box mb={10}>{children}</Box>;
 }
 
