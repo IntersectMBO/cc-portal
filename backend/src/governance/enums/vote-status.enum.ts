f05702907a0882da800fe75f13dd2c9d64b3c51a
bedev24 <15165767+milos1991@users.noreply.github.com>
2024-07-04 15:25:44 +0200

    feature/gap-search-api: lowercased vote statusees

--- backend/src/governance/enums/vote-status.enum.ts
+++ backend/src/governance/enums/vote-status.enum.ts
@@ -2,13 +2,13 @@ export enum VoteStatus {
   /**
    * Unvoted - There are neither votes nor reasoning for the associated GAP for a particular user
    */
-  Unvoted = 'Unvoted',
+  Unvoted = 'unvoted',
   /**
    * Pending - There is no vote but there is a reasoning for the associated GAP for a particular user
    */
-  Pending = 'Pending',
+  Pending = 'pending',
   /**
    * Voted - There are both votes and a reasoning for the associated GAP for a particular user
    */
-  Voted = 'Voted',
+  Voted = 'voted',
 }
