681fe8b1f00ce83a1487d6fadd3839c6183fab33
bedev24 <15165767+milos1991@users.noreply.github.com>
2024-07-07 17:22:11 +0200

    feature/gap-search-api: Added filter for vote and reasoning

--- backend/src/governance/util/pagination/gap-pagination.config.ts
+++ backend/src/governance/util/pagination/gap-pagination.config.ts
@@ -3,8 +3,11 @@ import { GovActionProposal } from 'src/governance/entities/gov-action-proposal.e
 
 export const GOVERNANCE_ACTION_PROPOSAL_CONFIG: PaginateConfig<GovActionProposal> =
   {
+    relations: ['votes', 'reasonings'],
     sortableColumns: ['endTime', 'submitTime'],
     filterableColumns: {
+      'votes.userId': [FilterOperator.EQ],
+      'reasonings.userId': [FilterOperator.EQ],
       govActionType: [FilterOperator.EQ, FilterOperator.IN],
       status: [FilterOperator.EQ, FilterOperator.IN],
     },
