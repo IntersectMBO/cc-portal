5d7f7a906bd9450884b437157d6b417900333770
Milos <161627443+BEdev24@users.noreply.github.com>
2024-08-26 14:25:06 +0200

    fix/increase-max-event-listenrs: incresed to 50 (#307)

--- ipfs-service/src/main.ts
+++ ipfs-service/src/main.ts
@@ -1,7 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { EventEmitter } from 'events';
 
 async function bootstrap() {
+  // Increase the max listeners for EventEmitters
+  EventEmitter.defaultMaxListeners = 50; // Set this to the desired number
+  
   const app = await NestFactory.create(AppModule);
   await app.listen(3001);
 }
