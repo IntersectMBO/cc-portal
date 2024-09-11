ea9bc401e2a0a7de3c7a97a6e7399de166e922f5
Milos <161627443+BEdev24@users.noreply.github.com>
2024-09-11 12:21:53 +0200

    refactor: add redis username
    Added username env variable within backend/worker-service/ipfs-service

--- backend/src/redis/client/redis-client.ts
+++ backend/src/redis/client/redis-client.ts
@@ -9,6 +9,7 @@ export const redisClientFactory: FactoryProvider<Redis> = {
     const redisInstance = new Redis({
       host: configService.getOrThrow('REDIS_HOST'),
       port: configService.getOrThrow('REDIS_PORT'),
+      username: configService.getOrThrow('REDIS_USERNAME'),
       password: configService.getOrThrow('REDIS_PASSWORD'),
     });
 
