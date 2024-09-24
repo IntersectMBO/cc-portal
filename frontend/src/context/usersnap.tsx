5cd4817eeff3bf5ee30a8e72b92ef4b7be4b1c81
Kristina <kristinapejcic1994@gmail.com>
2024-09-24 10:00:17 +0200

    fix: Usersnap multiple buttons appears onn page change

--- frontend/src/context/usersnap.tsx
+++ frontend/src/context/usersnap.tsx
@@ -1,36 +1,37 @@
 "use client";
 import React, { useEffect, useState, useContext } from "react";
+import { usePathname } from "next/navigation";
 import { InitOptions, loadSpace, SpaceApi } from "@usersnap/browser";
 
 export const UsersnapContext = React.createContext<SpaceApi | null>(null);
 
-// This context is used to integrate the Usersnap widget,
-// Usersnap is a tool for collecting user feedback and bug reports directly from the application
 export const UsersnapProvider = ({
   initParams,
   children,
 }: UsersnapProviderProps) => {
   const [usersnapApi, setUsersnapApi] = useState<SpaceApi | null>(null);
+  const pathname = usePathname();
 
   useEffect(() => {
     if (process.env.NEXT_PUBLIC_USERSNAP_SPACE_API_KEY) {
-      const hiddenProjects = process.env.NEXT_PUBLIC_HIDDEN_USERSNAP_PROJECT_IDS?.split("||") || []
+      const hiddenProjects =
+        process.env.NEXT_PUBLIC_HIDDEN_USERSNAP_PROJECT_IDS?.split("||") || [];
       loadSpace(process.env.NEXT_PUBLIC_USERSNAP_SPACE_API_KEY).then(
         (api: SpaceApi) => {
           api.init(initParams);
           setUsersnapApi(api);
           const hideHiddenProjects = () => {
-            hiddenProjects.forEach(p => {
+            hiddenProjects.forEach((p) => {
               api.hide(p);
             });
           };
           hideHiddenProjects();
-          api.on("close", hideHiddenProjects)
-          api.on("submit", hideHiddenProjects)
+          api.on("close", hideHiddenProjects);
+          api.on("submit", hideHiddenProjects);
         }
       );
     }
-  }, [initParams]);
+  }, [initParams, pathname]); // Run effect on pathname change
 
   return (
     <UsersnapContext.Provider value={usersnapApi}>
