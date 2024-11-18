7fb8d30e57b176de289b261e0db8c18bc7bf9e97
Milos <developer@bloxico.com>
2024-11-18 11:14:36 +0100

    feature/reationale-cip-136: polish unit test

--- backend/src/governance/api/request/rationale.request.ts
+++ backend/src/governance/api/request/rationale.request.ts
@@ -1,13 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, MaxLength } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class RationaleRequest {
   @ApiProperty({ description: 'Summary' })
+  @IsNotEmpty()
   @MaxLength(200, { message: `Maximum character length is 200` })
   @IsString()
   summary: string;
 
   @ApiProperty({ description: 'Rationale statement' })
+  @IsNotEmpty()
   @IsString()
   rationaleStatement: string;
 }
