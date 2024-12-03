a62b206da361ef2ec69df0de200576c0c02cb20f
Milos <161627443+BEdev24@users.noreply.github.com>
2024-12-03 14:58:26 +0100

    Revert "feat: change users role and permissions (#475)"
    This reverts commit e32957e9b97bd178943dfffead292f9b0e3f6977.

--- backend/src/users/enums/permission.enum.ts
+++ backend/src/users/enums/permission.enum.ts
@@ -2,5 +2,4 @@ export enum PermissionEnum {
   MANAGE_CC_MEMBERS = 'manage_cc_members',
   ADD_CONSTITUTION = 'add_constitution_version',
   MANAGE_ADMINS = 'manage_admins',
-  MANAGE_ROLES_AND_PERMISSIONS = 'manage_roles_and_permissions',
 }
