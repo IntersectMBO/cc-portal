e0629e7c45e6532d1ad2c50c1bf00b4ec9a21dcb
nike-getto <savastanisic7@gmail.com>
2024-12-04 22:35:09 +0100

    conflict-resolving

--- frontend/src/components/organisms/Constitution/MDXComponents.tsx
+++ frontend/src/components/organisms/Constitution/MDXComponents.tsx
@@ -16,7 +16,7 @@ const Anchor = ({ id, offset = "-20vh " }) => {
         display: "block",
         position: "relative",
         top: offset,
-        visibility: "hidden"
+        visibility: "hidden",
       }}
     />
   );
@@ -36,7 +36,7 @@ export const Heading1 = ({ children, id }) => (
         marginTop: "24px",
         marginBottom: "16px",
         lineHeight: "1.25em",
-        fontSize: { xxs: 20, md: 32 }
+        fontSize: { xxs: 20, md: 32 },
       }}
       variant="headline4"
     >
@@ -55,7 +55,7 @@ export const Heading2 = ({ children, id }) => (
         fontWeight: 600,
         fontSize: { xxs: 16, md: 20 },
 
-        lineHeight: "1.25em"
+        lineHeight: "1.25em",
       }}
     >
       {children}
@@ -72,7 +72,7 @@ export const Heading3 = ({ children, id }) => (
         marginBottom: "16px",
         fontWeight: 600,
         fontSize: { xxs: 14, md: 18 },
-        lineHeight: "1.25em"
+        lineHeight: "1.25em",
       }}
     >
       {children}
@@ -89,11 +89,7 @@ export const Heading5 = ({ children }) => (
       fontWeight: 800,
       fontSize: { xxs: 12, md: 14 },
       lineHeight: "1em",
-<<<<<<< HEAD
-      overflowWrap: "break-word"
-=======
       overflowWrap: "break-word",
->>>>>>> staging
     }}
   >
     {children}
@@ -107,11 +103,7 @@ export const Paragraph = ({ children, id }) => (
         lineHeight: "1.5",
         marginBottom: "16px",
         fontSize: "14px",
-<<<<<<< HEAD
-        color: customPalette.textGray
-=======
         color: customPalette.textGray,
->>>>>>> staging
       }}
       variant="caption"
     >
@@ -130,11 +122,7 @@ export const ListItem = ({ children, id }) => (
       flexDirection: "column",
       alignItems: "flex-start",
       justifyContent: "center",
-<<<<<<< HEAD
-      wordBreak: "break-all"
-=======
       wordBreak: "break-all",
->>>>>>> staging
     }}
   >
     {children}
@@ -150,7 +138,7 @@ export const Code = ({ children }) => (
       margin: 0,
       whiteSpace: "break-spaces",
       backgroundColor: "#afb8c133",
-      borderRadius: "6px"
+      borderRadius: "6px",
     }}
   >
     {children}
@@ -162,7 +150,7 @@ export const TABLE_OF_CONTENTS_WRAPPER_STYLE_PROPS = {
   borderRadius: "16px",
   padding: "12px",
   "& ol.toc-level": {
-    margin: 0
+    margin: 0,
   },
   "& ol.toc-level-1": {
     paddingInlineStart: "20px",
@@ -170,12 +158,12 @@ export const TABLE_OF_CONTENTS_WRAPPER_STYLE_PROPS = {
     "& li": {
       listStyle: "outside !important",
       "& a.toc-link-h1": {
-        fontWeight: 600
-      }
-    }
+        fontWeight: 600,
+      },
+    },
   },
   "& ol.toc-level-2": {
-    margin: "10px 0px 10px 0px"
+    margin: "10px 0px 10px 0px",
   },
   "& li": {
     width: "100%",
@@ -186,15 +174,9 @@ export const TABLE_OF_CONTENTS_WRAPPER_STYLE_PROPS = {
       fontSize: "1rem",
       fontWeight: 400,
       lineHeight: "56px",
-<<<<<<< HEAD
-      color: customPalette.textBlack
-    }
-  }
-=======
       color: customPalette.textBlack,
     },
   },
->>>>>>> staging
 };
 export const DrawerNav = () => {
   const t = useTranslations("Constitution");
@@ -225,11 +207,7 @@ export const NavDrawerDesktop = ({
   children,
   left = 0,
   top = { xxs: 75, md: 90 },
-<<<<<<< HEAD
-  dataTestId
-=======
   dataTestId,
->>>>>>> staging
 }: {
   children: ReactNode;
   left: number;
@@ -250,7 +228,7 @@ export const NavDrawerDesktop = ({
         borderRadius: "16px",
         height: { xxs: "95vh", md: "calc(100vh - 118px)" },
         zIndex: 1,
-        ...TABLE_OF_CONTENTS_WRAPPER_STYLE_PROPS
+        ...TABLE_OF_CONTENTS_WRAPPER_STYLE_PROPS,
       }}
     >
       <Box
@@ -259,13 +237,8 @@ export const NavDrawerDesktop = ({
           overflowY: "scroll",
           borderRadiusTop: "16px 16px 0 0",
           "&::-webkit-scrollbar": {
-<<<<<<< HEAD
-            display: "none"
-          }
-=======
             display: "none",
           },
->>>>>>> staging
         }}
         data-testid="nav-drawer-collapse-container"
       >
@@ -283,11 +256,7 @@ export const NavCard = ({
   hash,
   url,
   isActive,
-<<<<<<< HEAD
-  isLatest
-=======
   isLatest,
->>>>>>> staging
 }) => {
   return (
     <Grid
@@ -347,7 +316,7 @@ export const NavCard = ({
           sx={{
             display: "flex",
             justifyContent: "center",
-            alignItems: "center"
+            alignItems: "center",
           }}
         >
           <Link
@@ -372,7 +341,7 @@ export const NavCard = ({
         sx={{
           display: "flex",
           justifyContent: "center",
-          alignItems: "center"
+          alignItems: "center",
         }}
       >
         {!isActive && !isLatest && (
