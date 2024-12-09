bff3cd6926421e39ce3606c5ca2efe07871bdce3
Milos <161627443+BEdev24@users.noreply.github.com>
2024-12-09 15:24:14 +0100

    feat: update helia and add job for pruning peer store (#496)
    * feature/update-helia: updated helia and added job for pruning peer store
    
    * feature/update-helia: minor changes

--- ipfs-service/src/app.module.ts
+++ ipfs-service/src/app.module.ts
@@ -6,16 +6,43 @@ import { BullModule } from '@nestjs/bullmq';
 import { BullmqModule } from './bullmq/bullmq.module.js';
 import { ProvideToDHTProcessor } from './queues/processors/provide-to-dht.processor.js';
 import { ProvideToDHTProducer } from './queues/producers/provide-to-dht.producer.js';
-import { QUEUE_NAME_PROVIDE_TO_DHT } from './constants/bullmq.constants.js';
+import { QUEUE_NAME_PROVIDE_TO_DHT, QUEUE_NAME_PRUNE_PEER_STORE } from './constants/bullmq.constants.js';
+import { PrunePeerStoreProducer } from './queues/producers/prune-peer-store.producer.js';
+import { PrunePeerStoreProcessor } from './queues/processors/prune-peer-store.processor.js';
+import { PrunePeerStoreJob } from './jobs/impl/prune-peer-store.job.js';
+import { Scheduler } from './jobs/scheduler.js';
+import { SchedulerRegistry } from '@nestjs/schedule';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     BullmqModule,
     BullModule.registerQueue({
-    name: QUEUE_NAME_PROVIDE_TO_DHT,
-  }),],
+      name: QUEUE_NAME_PROVIDE_TO_DHT,
+    }),
+    BullModule.registerQueue({
+      name: QUEUE_NAME_PRUNE_PEER_STORE,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService, ProvideToDHTProcessor, ProvideToDHTProducer],
+  providers: [
+    AppService,
+    ProvideToDHTProcessor,
+    ProvideToDHTProducer,
+    PrunePeerStoreProducer,
+    PrunePeerStoreProcessor,
+    SchedulerRegistry,
+    Scheduler,
+    PrunePeerStoreJob,
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(
+    private readonly scheduler: Scheduler,
+    private readonly prunePeerStoreJob: PrunePeerStoreJob,
+  ) {}
+
+  onModuleInit() {
+    this.scheduler.registerJob(this.prunePeerStoreJob);
+  }
+}
