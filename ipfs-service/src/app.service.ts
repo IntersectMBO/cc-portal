a3325f5b43c7f4ef0c97b65685461b51119dbd75
Milos <161627443+BEdev24@users.noreply.github.com>
2024-12-17 11:27:08 +0100

    fix/ipfs-amino-dht: changed peer discovery bootstrap list for amino DHT (#500)

--- ipfs-service/src/app.service.ts
+++ ipfs-service/src/app.service.ts
@@ -18,7 +18,7 @@ import { FsBlockstore } from 'blockstore-fs';
 import { LevelDatastore } from 'datastore-level';
 import { IPNS, ipns } from '@helia/ipns';
 import { keychain, type Keychain } from '@libp2p/keychain';
-import { kadDHT } from '@libp2p/kad-dht';
+import { kadDHT, removePrivateAddressesMapper } from '@libp2p/kad-dht';
 import { ipnsSelector } from 'ipns/selector';
 import { ipnsValidator } from 'ipns/validator';
 import { dcutr } from '@libp2p/dcutr';
@@ -64,13 +64,13 @@ const libp2pOptions = {
    mdns(),
     bootstrap({
       list: [
-        '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
-        '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
-        '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt',
-        '/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ',
-        '/ip4/104.131.131.82/udp/4001/quic-v1/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ',
-        '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
-        '/dnsaddr/bootstrap.libp2p.io/p2p/QmZa1sAxajnQjVM8WjWXoMbmPd7NsWhfKsPkErzpm9wGkp',
+        '/dns4/am6.bootstrap.libp2p.io/tcp/443/wss/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+        '/dns4/sg1.bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt',
+        '/dns4/sv15.bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+        // va1 is not in the TXT records for _dnsaddr.bootstrap.libp2p.io yet
+        // so use the host name directly
+        '/dnsaddr/va1.bootstrap.libp2p.io/p2p/12D3KooWKnDdG3iXw9eTFijk3EWSunZcFi54Zka4wmtqtt6rPxc8',
+        '/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ'
       ],
     }),
   ],
@@ -79,13 +79,10 @@ const libp2pOptions = {
     dcutr: dcutr(),
     delegatedRouting: () =>
       createDelegatedRoutingV1HttpApiClient('https://delegated-ipfs.dev'),
-    dht: kadDHT({
+    aminoDHT: kadDHT({
       clientMode: false,
-      initialQuerySelfInterval: 1000,
-      kBucketSize: 20,
+      peerInfoMapper: removePrivateAddressesMapper,
       protocol: '/ipfs/kad/1.0.0',
-      maxInboundStreams: 32,
-      maxOutboundStreams: 64,
       validators: { ipns: ipnsValidator },
       selectors: { ipns: ipnsSelector },
     }),
