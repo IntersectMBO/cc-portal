f17cb09c63e068acba8cce35923dc6cce47d1387
Milos <developer@bloxico.com>
2025-01-04 12:56:56 +0100

    fix/announce-cid-with-cronjob: Provided announcing cids with cronjob process

--- ipfs-service/src/constants/bullmq.constants.ts
+++ ipfs-service/src/constants/bullmq.constants.ts
@@ -1,5 +1,8 @@
-export const QUEUE_NAME_PROVIDE_TO_DHT = "provide_to_dht_queue";
-export const JOB_NAME_PROVIDE_TO_DHT = "provide_to_dht_job";
+export const QUEUE_NAME_PROVIDE_TO_DHT = 'provide_to_dht_queue';
+export const JOB_NAME_PROVIDE_TO_DHT = 'provide_to_dht_job';
 
-export const QUEUE_NAME_PRUNE_PEER_STORE = "prune_peer_store_queue";
-export const JOB_NAME_PRUNE_PEER_STORE = "prune_peer_store_job";
+export const QUEUE_NAME_PRUNE_PEER_STORE = 'prune_peer_store_queue';
+export const JOB_NAME_PRUNE_PEER_STORE = 'prune_peer_store_job';
+
+export const QUEUE_NAME_PROVIDE_ALL_CIDS = 'provide_all_cids_queue';
+export const JOB_NAME_PROVIDE_ALL_CIDS = 'provide_all_cids_job';
