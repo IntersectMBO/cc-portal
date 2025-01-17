a90245506122c40a7ad0ee200bca6082af687ea4
Milos <161627443+BEdev24@users.noreply.github.com>
2025-01-17 14:36:45 +0100

    fix: announce cid with cronjob (#529)
    * fix/announce-cid-with-cronjob: Provided announcing cids with cronjob process
    
    * fix-announce-cid-with-cronjob: changed datastore to datastore-fs and added custom libp2p
    
    * fix/announce-cid-with-cronjob: resolved conflicts

--- ipfs-service/src/main.ts
+++ ipfs-service/src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module.js';
 import morgan from 'morgan';
 import { LoggerFactory } from './util/logger-factory.js';
 import { LoggerService } from '@nestjs/common';
-import events from 'events';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,9 +20,6 @@ async function bootstrap() {
       },
     }),
   );
-
-  // Increase the limit globally for all event emitters
-  events.EventEmitter.defaultMaxListeners = 50; // Set your desired limit
 
   await app.listen(3001);
 }
